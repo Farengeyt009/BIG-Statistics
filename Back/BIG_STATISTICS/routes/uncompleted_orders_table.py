5fb25d904a8d9e9d0e9e0ee69042bdca43bc0b9c
Farengeyt009 <your-email@example.com>
2025-06-27 09:38:31 +0800

    chore: remove legacy Back folder

--- Back/BIG_STATISTICS/routes/uncompleted_orders_table.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from ..services.uncompleted_orders_table import get_all_uncompleted_orders_table
-
-uncompleted_orders_table_bp = Blueprint('uncompleted_orders_table', __name__)
-
-@uncompleted_orders_table_bp.route('/api/uncompleted-orders/table', methods=['GET'])
-def get_table():
-    result = get_all_uncompleted_orders_table()
-    return jsonify(result)
--- Back/BIG_STATISTICS/routes/uncompleted_orders_views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-from ..services.uncompleted_orders_views import get_uncompleted_orders_views
-
-uncompleted_orders_views_bp = Blueprint('uncompleted_orders_views', __name__)
-
-@uncompleted_orders_views_bp.route('/api/uncompleted-orders/views', methods=['GET'])
-def uncompleted_orders_views():
-    return jsonify(get_uncompleted_orders_views())
