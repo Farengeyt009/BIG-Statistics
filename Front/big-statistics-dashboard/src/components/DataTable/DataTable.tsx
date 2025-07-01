426f060ba1aa8231f4123585e9d1edc844c1acbd
Farengeyt009 <your-email@example.com>
2025-07-01 16:22:28 +0800

    Обновление: синхронизация всех изменений с GitHub

--- Front/big-statistics-dashboard/src/components/DataTable/DataTable.tsx
+++ Front/big-statistics-dashboard/src/components/DataTable/DataTable.tsx
@@ -190,7 +190,7 @@ export function DataTable<T extends Record<string, any>>({
       }}
     >
       <div ref={parentRef} data-testid="scroll-area" className="max-h-96 overflow-auto">
-        <table className="min-w-full text-sm border table-fixed w-full">
+      <table className="min-w-max text-sm border table-auto">
           <thead className="bg-gray-100">
             <SortableContext items={columnOrder}>
               {table.getHeaderGroups().map((hg) => (
